acaa28e79cbad152ea3bec530fb0ac936d884274
pandaman64 <kointosudesuyo@infoseek.jp>
2018-06-15 14:15:09 +0900

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use std::collections::BTreeMap;
 
 extern crate rand;
-use rand::prelude::*;
 use rand::distributions::Standard;
+use rand::prelude::*;
 
 const INITIAL_WIDTH: usize = 5;
 const STEP: usize = 2;
@@ -23,17 +23,18 @@ impl<SiteId: Ord> std::cmp::Ord for Key<SiteId> {
 
         let mut lhs = self.0.iter();
         let mut rhs = other.0.iter();
-        
+
         for _ in 0..max_level {
-            let left = lhs.next()
+            let left = lhs
+                .next()
                 .map(|item| (item.0, &item.1))
                 .unwrap_or_else(|| (0, &Id::Sentinel));
-            let right = rhs.next()
+            let right = rhs
+                .next()
                 .map(|item| (item.0, &item.1))
                 .unwrap_or_else(|| (0, &Id::Sentinel));
 
-            let cmp = left.0.cmp(&right.0)
-                .then_with(|| left.1.cmp(&right.1));
+            let cmp = left.0.cmp(&right.0).then_with(|| left.1.cmp(&right.1));
             if cmp != Equal {
                 return cmp;
             }
@@ -49,9 +50,14 @@ impl<SiteId: Ord> std::cmp::PartialOrd for Key<SiteId> {
     }
 }
 
-fn pick_index<R: Rng + ?Sized>(rng: &mut R, left: usize, right: usize, strategy: InsertionStrategy) -> usize {
+fn pick_index<R: Rng + ?Sized>(
+    rng: &mut R,
+    left: usize,
+    right: usize,
+    strategy: InsertionStrategy,
+) -> usize {
     use InsertionStrategy::*;
-    
+
     // [left, right) must have at least one element to pick
     assert!(left + 1 <= right);
 
@@ -61,13 +67,13 @@ fn pick_index<R: Rng + ?Sized>(rng: &mut R, left: usize, right: usize, strategy:
             let right = left.saturating_add(STEP).min(right);
 
             rng.gen_range(left, right)
-        },
+        }
         Back => {
             let right = right;
             let left = right.saturating_sub(STEP).max(left);
 
             rng.gen_range(left, right)
-        },
+        }
     }
 }
 
@@ -75,7 +81,11 @@ fn width(level: usize) -> usize {
     INITIAL_WIDTH * 2_usize.pow(level as u32)
 }
 
-fn get_strategy<R: Rng + ?Sized>(rng: &mut R, strategies: &mut Vec<InsertionStrategy>, level: usize) -> InsertionStrategy {
+fn get_strategy<R: Rng + ?Sized>(
+    rng: &mut R,
+    strategies: &mut Vec<InsertionStrategy>,
+    level: usize,
+) -> InsertionStrategy {
     for _ in strategies.len()..=level {
         strategies.push(rng.gen());
     }
@@ -83,34 +93,40 @@ fn get_strategy<R: Rng + ?Sized>(rng: &mut R, strategies: &mut Vec<InsertionStra
 }
 
 impl<SiteId: Ord + Clone> Key<SiteId> {
-    fn pick(&self, other: &Self, site_id: Id<SiteId>, strategies: &mut Vec<InsertionStrategy>) -> Self {
+    fn pick(
+        &self,
+        other: &Self,
+        site_id: Id<SiteId>,
+        strategies: &mut Vec<InsertionStrategy>,
+    ) -> Self {
         assert!(*self < *other);
-        
+
         let mut rng = rand::thread_rng();
         let mut lhs = self.0.iter();
         let mut rhs = other.0.iter();
         let mut ret = vec![];
         let mut level = 0;
 
-        let pos =
-            loop {
-                let (lpos, lid) = lhs.next()
-                    .map(|x| (x.0, &x.1))
-                    .unwrap_or_else(|| (0, &Id::Sentinel));
-                let (rpos, _) = rhs.next()
-                    .map(|x| (x.0, &x.1))
-                    .unwrap_or_else(|| (width(level), &Id::Sentinel));
+        let pos = loop {
+            let (lpos, lid) = lhs
+                .next()
+                .map(|x| (x.0, &x.1))
+                .unwrap_or_else(|| (0, &Id::Sentinel));
+            let (rpos, _) = rhs
+                .next()
+                .map(|x| (x.0, &x.1))
+                .unwrap_or_else(|| (width(level), &Id::Sentinel));
 
-                if lpos + 1 < rpos {
-                    let strategy = get_strategy(&mut rng, strategies, level);
-                    break pick_index(&mut rng, lpos + 1, rpos, strategy);
-                } else if lpos + 1 == rpos && *lid < site_id {
-                    break lpos;
-                } else {
-                    level += 1;
-                    ret.push((lpos, lid.clone()));
-                }
-            };
+            if lpos + 1 < rpos {
+                let strategy = get_strategy(&mut rng, strategies, level);
+                break pick_index(&mut rng, lpos + 1, rpos, strategy);
+            } else if lpos + 1 == rpos && *lid < site_id {
+                break lpos;
+            } else {
+                level += 1;
+                ret.push((lpos, lid.clone()));
+            }
+        };
         ret.push((pos, site_id));
         Key(ret)
     }
@@ -143,32 +159,48 @@ pub struct Document<SiteId, Value> {
 impl<SiteId: Ord + Clone + std::fmt::Debug, Value> Document<SiteId, Value> {
     pub fn new() -> Self {
         let mut content = BTreeMap::new();
-        content.insert(Key(vec![ (0, Id::Sentinel) ]), None);
-        content.insert(Key(vec![ (INITIAL_WIDTH, Id::Sentinel) ]), None);
+        content.insert(Key(vec![(0, Id::Sentinel)]), None);
+        content.insert(Key(vec![(INITIAL_WIDTH, Id::Sentinel)]), None);
 
         Document {
             content: content,
-            strategies: vec![ rand::random() ],
+            strategies: vec![rand::random()],
             current_level: 0,
         }
     }
 
     pub fn start(&self) -> Key<SiteId> {
-        Key(vec![ (0, Id::Sentinel) ])
+        Key(vec![(0, Id::Sentinel)])
     }
 
     pub fn end(&self) -> Key<SiteId> {
-        Key(vec![ (INITIAL_WIDTH, Id::Sentinel) ])
+        Key(vec![(INITIAL_WIDTH, Id::Sentinel)])
     }
 
-    pub fn insert(&mut self, site_id: SiteId, left: &Key<SiteId>, right: &Key<SiteId>, value: Value) -> Key<SiteId> {
+    pub fn insert(
+        &mut self,
+        site_id: SiteId,
+        left: &Key<SiteId>,
+        right: &Key<SiteId>,
+        value: Value,
+    ) -> Key<SiteId> {
         let key = left.pick(right, Id::Site(site_id), &mut self.strategies);
-        assert!(left < &key && &key < right, "must hold {:?} < {:?} < {:?}", left, key, right);
-        assert!(self.content.insert(key.clone(), Some(value)).is_none(), "key collided: {:?}", key);
+        assert!(
+            left < &key && &key < right,
+            "must hold {:?} < {:?} < {:?}",
+            left,
+            key,
+            right
+        );
+        assert!(
+            self.content.insert(key.clone(), Some(value)).is_none(),
+            "key collided: {:?}",
+            key
+        );
         key
     }
 
-    pub fn iter<'a>(&'a self) -> impl Iterator<Item = (&'a Value)> {
+    pub fn iter<'a>(&'a self) -> impl Iterator<Item = &'a Value> {
         let start = self.start();
         let end = self.end();
         self.content
@@ -184,14 +216,11 @@ mod test {
 
     #[test]
     fn test_equality() {
-        let key = Key(vec![
-            (1, Id::Site(())),
-            (4, Id::Site(())),
-        ]);
+        let key = Key(vec![(1, Id::Site(())), (4, Id::Site(()))]);
         assert_eq!(key, key);
     }
 
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)] 
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
     enum Characters {
         Alice,
         Bob,
@@ -223,11 +252,7 @@ mod test {
                     left = iter.next().unwrap();
                     right = iter.next().unwrap();
                 }
-                new_key = doc.insert(if rng.gen() {
-                    Alice
-                } else {
-                    Bob
-                }, left, right, rng.gen());
+                new_key = doc.insert(if rng.gen() { Alice } else { Bob }, left, right, rng.gen());
             }
 
             keys.insert(new_key);
